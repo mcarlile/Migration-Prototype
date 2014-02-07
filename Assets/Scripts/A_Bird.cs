239a9b1fcf346a60b62d51c84be1089e9a92cefa
Alexis Miller <anmiller@usc.edu>
2014-02-06 20:56:10 -0800

    Version 1.3
    Combined prototype projects

---
+++ Assets/Scripts/A_Bird.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class A_Bird : MonoBehaviour {
+	
+//	public GameObject prefab;
+//	GameObject flock = Instantiate(prefab);
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	
+	}
+	
+	void OnTriggerEnter(Collider collision) {
+		
+		if (collision.gameObject.name.Contains("Obsticle")) {
+			Debug.Log("Hit obsticle"); 
+			Destroy(gameObject);
+
+
+		}
+
+
+		if (collision.name.Contains("EndGame")) {
+
+		}
+		
+	}
+}
---
+++ Assets/Scripts/A_Flock.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class A_Flock : MonoBehaviour {
+
+	private Vector3 movement = new Vector3 (0.0f, 0.0f, 0.0f);
+	public float movementSpeed = 1f;
+
+	public GameObject bird1;
+	public GameObject bird2;
+	public GameObject bird3;
+	public GameObject bird4;
+	public GameObject bird5;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		movement = Vector3.forward * movementSpeed * Time.deltaTime;
+		gameObject.transform.Translate (movement);
+
+		if (Input.GetKey(KeyCode.RightArrow)) {
+			movement = Vector3.right * movementSpeed * Time.deltaTime;
+			gameObject.transform.Translate (movement);
+		}
+
+		if (Input.GetKey(KeyCode.LeftArrow)) {
+			movement = Vector3.left * movementSpeed * Time.deltaTime;
+			gameObject.transform.Translate (movement);
+		}
+
+	}
+
+
+}
+
+
+
+
+

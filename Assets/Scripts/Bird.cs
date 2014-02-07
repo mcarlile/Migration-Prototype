56d61bf8dc71b943e55f3f6e1475d057f68881c5
mcarlile <mckenzie.carlile@gmail.com>
2014-02-06 20:26:22 -0800

    Version 1.0
    Created repo and moved existing project files in

---
+++ Assets/Scripts/Bird.cs
@@ -0,0 +1,143 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bird : MonoBehaviour
+{
+		public GameObject flock;
+		public GameObject health1;
+		public GameObject health2;
+		public GameObject health3;
+		public GameObject health4;
+		public GameObject health5;
+		public GameObject health6;
+		public GameObject health7;
+		public GameObject light;
+		public Color selectedColor;
+		public Color deselectedColor;
+
+		public float movementSpeed = 1.0f;
+
+		public int health = 7;
+		public int position;
+
+		private bool dead = false;
+		public bool selected = false;
+		public bool allowClick = true;
+
+
+		private Vector3 movement = new Vector3 (0.0f, 0.0f, 0.0f);
+	
+		// Use this for initialization
+		void Start ()
+		{
+				flock = GameObject.Find ("Flock");
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+				Debug.Log ("Name: " + gameObject.transform.name);
+
+				if (health < 7) {
+						health7.SetActive (false);
+				} else {
+						health7.SetActive (true);
+				}
+
+				if (health < 6) {
+						health6.SetActive (false);
+				} else {
+						health6.SetActive (true);
+				}
+
+				if (health < 5) {
+						health5.SetActive (false);
+				} else {
+						health5.SetActive (true);
+				}
+
+				if (health < 4) {
+						health4.SetActive (false);
+				} else {
+						health4.SetActive (true);
+				}
+
+				if (health < 3) {
+						health3.SetActive (false);
+				} else {
+						health3.SetActive (true);
+				}
+
+				if (health < 2) {
+						health2.SetActive (false);
+				} else {
+						health2.SetActive (true);
+				}
+
+				if (health < 1) {
+						health1.SetActive (false);
+				} else {
+						health1.SetActive (true);
+				}
+
+				if (selected == (true)) {
+						gameObject.renderer.material.SetColor ("_Color", selectedColor);
+				} else {
+						gameObject.renderer.material.SetColor ("_Color", deselectedColor);
+				}
+		}
+
+		public void OnTriggerEnter (Collider  otherCollider)
+		{
+		
+				if (otherCollider.gameObject.name.Contains ("Forward")) {
+						position = 1;
+				}
+		
+				if (otherCollider.gameObject.name.Contains ("Middle")) {
+						position = 2;
+				}
+		
+				if (otherCollider.gameObject.name.Contains ("Rear")) {
+						position = 3;
+				}
+		}
+		public void ChangeHealth ()
+		{
+				if ((position == 1) && (health <= 7)) {
+						health = health - 1;
+				}
+
+				if ((position == 2) && (health <= 7)) {
+				}
+
+				if ((position == 3) && (health <= 7)) {
+						health = health + 1;
+				}
+		}
+		void OnMouseOver ()
+		{
+				light.SetActive (true);
+
+				if (allowClick == (true)) {
+						
+						if ((Input.GetMouseButtonDown (0))) {
+								selected = !selected;
+								allowClick = false;
+								flock.GetComponent<Flock> ().ToggleBirdSelected (gameObject.transform.name);
+						}
+				}
+		}
+
+
+
+		void OnMouseExit ()
+		{
+				light.SetActive (false);
+				allowClick = true;
+
+		}
+	
+
+}
+
---
+++ Assets/Scripts/Flock.cs
@@ -0,0 +1,98 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Flock : MonoBehaviour
+{
+
+		private Vector3 movement = new Vector3 (0.0f, 0.0f, 0.0f);
+		public float movementSpeed = 1.0f;
+		public int health = 7;
+		public GameObject bird1;
+		public GameObject bird2;
+		public GameObject bird3;
+		public GameObject bird4;
+		public GameObject bird5;
+		public bool triggered = false;
+		public bool birdSelected;
+		public string specificBirdSelected;
+
+
+		// Use this for initialization
+		void Start ()
+		{
+	
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+	
+				Debug.Log ("Specific Bird Selected: " + specificBirdSelected);
+
+				//Movement
+				movement = Vector3.up * movementSpeed * Time.deltaTime;
+				gameObject.transform.Translate (movement);
+
+				if (specificBirdSelected == ("Bird1")) {
+						bird2.gameObject.GetComponent<Bird> ().allowClick = false;
+						bird3.gameObject.GetComponent<Bird> ().allowClick = false;
+						bird4.gameObject.GetComponent<Bird> ().allowClick = false;
+						bird5.gameObject.GetComponent<Bird> ().allowClick = false;
+				}
+
+				if (specificBirdSelected == ("Bird2")) {
+						bird1.gameObject.GetComponent<Bird> ().allowClick = false;
+						bird3.gameObject.GetComponent<Bird> ().allowClick = false;
+						bird4.gameObject.GetComponent<Bird> ().allowClick = false;
+						bird5.gameObject.GetComponent<Bird> ().allowClick = false;
+				}
+
+				if (specificBirdSelected == ("Bird3")) {
+						bird2.gameObject.GetComponent<Bird> ().allowClick = false;
+						bird1.gameObject.GetComponent<Bird> ().allowClick = false;
+						bird4.gameObject.GetComponent<Bird> ().allowClick = false;
+						bird5.gameObject.GetComponent<Bird> ().allowClick = false;
+				}
+
+				if (specificBirdSelected == ("Bird4")) {
+						bird2.gameObject.GetComponent<Bird> ().allowClick = false;
+						bird3.gameObject.GetComponent<Bird> ().allowClick = false;
+						bird1.gameObject.GetComponent<Bird> ().allowClick = false;
+						bird5.gameObject.GetComponent<Bird> ().allowClick = false;
+				}
+
+				if (specificBirdSelected == ("Bird5")) {
+						bird2.gameObject.GetComponent<Bird> ().allowClick = false;
+						bird3.gameObject.GetComponent<Bird> ().allowClick = false;
+						bird4.gameObject.GetComponent<Bird> ().allowClick = false;
+						bird1.gameObject.GetComponent<Bird> ().allowClick = false;
+				}
+		}
+
+		void OnTriggerEnter (Collider  otherCollider)
+		{
+				if ((otherCollider.gameObject.name.Contains ("ReductionZone")) && (triggered == false)) {
+						bird1.gameObject.GetComponent<Bird> ().ChangeHealth ();
+						bird2.gameObject.GetComponent<Bird> ().ChangeHealth ();
+						bird3.gameObject.GetComponent<Bird> ().ChangeHealth ();
+						bird4.gameObject.GetComponent<Bird> ().ChangeHealth ();
+						bird5.gameObject.GetComponent<Bird> ().ChangeHealth ();
+						triggered = true;
+				}				
+		}
+
+		void OnTriggerExit (Collider otherCollider)
+		{
+				if (otherCollider.gameObject.name.Contains ("ReductionZone")) {
+						triggered = false;
+
+				}		
+		}
+
+		public void ToggleBirdSelected (string birdName)
+		{
+				birdSelected = !birdSelected;
+				specificBirdSelected = birdName;
+		}
+	
+}

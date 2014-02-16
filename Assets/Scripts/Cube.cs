9cf93abdb04f4bfe46e28b8615e607fe84778f70
mcarlile <mckenzie.carlile@gmail.com>
2014-02-15 19:29:26 -0800

    Alpha 1.7
    Created ArrayTest to try to figure out a simpler way to handle bird
    movement

---
+++ Assets/Scripts/Cube.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cube : MonoBehaviour
+{
+		public Color highlightedColor;
+		public Color deselectedColor;
+		public GameObject manager;
+		public int cubeNumber;
+
+
+		// Use this for initialization
+		void Start ()
+		{
+				manager = GameObject.Find ("_Manager");
+				gameObject.renderer.material.SetColor ("_Color", deselectedColor);
+
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+	
+		}
+
+		void OnMouseOver ()
+		{
+				gameObject.renderer.material.SetColor ("_Color", highlightedColor);
+				if ((Input.GetMouseButtonDown (0))) {
+						manager.GetComponent<Manager> ().MoveCubeToFront (cubeNumber);
+				}
+		}
+
+		void OnMouseExit ()
+		{
+				gameObject.renderer.material.SetColor ("_Color", deselectedColor);
+
+		}
+}
---
+++ Assets/Scripts/Manager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Manager : MonoBehaviour
+{
+		public GameObject cube1;
+		public GameObject cube2;
+		public GameObject cube3;
+		public GameObject selectedCube;
+		public GameObject position1;
+		public GameObject position2;
+		public GameObject position3;
+
+		// Use this for initialization
+		void Start ()
+		{
+	
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+	
+		}
+
+		public void MoveCubeToFront (int cubeToMove)
+		{
+				if (cubeToMove == 1) {
+						iTween.MoveTo (cube1, iTween.Hash ("position", position2.transform.position, "easetype", iTween.EaseType.easeInOutSine, "time", 2f));
+				}
+
+				if (cubeToMove == 2) {
+						iTween.MoveTo (cube2, iTween.Hash ("position", position2.transform.position, "easetype", iTween.EaseType.easeInOutSine, "time", 2f));
+				}
+
+				if (cubeToMove == 3) {
+						iTween.MoveTo (cube3, iTween.Hash ("position", position2.transform.position, "easetype", iTween.EaseType.easeInOutSine, "time", 2f));
+				}
+		}
+}
